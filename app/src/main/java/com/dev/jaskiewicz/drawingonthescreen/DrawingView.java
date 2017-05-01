32f011eec98ef363d423f34d04b45c6cd2c82295
Michał Jaśkiewicz <michal.grzegorz.jaskiewicz@gmail.com>
2017-05-01 10:25:08 +0200

    samo rysowanie linii roznymi kolorami

---
+++ app/src/main/java/com/dev/jaskiewicz/drawingonthescreen/DrawingView.java
@@ -0,0 +1,84 @@
+package com.dev.jaskiewicz.drawingonthescreen;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+public class DrawingView extends View {
+
+    private Canvas canvas;
+    private Bitmap canvasBitmap;
+    private Path path;
+    private Paint canvasPaint;
+    private Paint paint;
+    private int defaultPaintColor = 0xFF000000;
+
+
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing() {
+        path = new Path();
+        paint = new Paint();
+        paint.setColor(defaultPaintColor);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(20);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(path, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                canvas.drawPath(path, paint);
+                path.reset();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
+    }
+
+    public void setPaintColor(int color) {
+        paint.setColor(color);
+    }
+
+    public void clear() {
+
+    }
+}
